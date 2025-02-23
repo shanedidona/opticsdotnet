a113e7494c6a0543fc2e06ad5321c383836b7949
Shane DiDona <shanedidona@gmail.com>
2025-02-22 22:00:29 -0500

    [ODN-1] public static string RenderMathematica(double double1)

--- opticsdotnet/opticsdotnet.Lib/MathematicaUtil.cs
+++ opticsdotnet/opticsdotnet.Lib/MathematicaUtil.cs
@@ -2,5 +2,9 @@
 {
     public static class MathematicaUtil
     {
+        public static string RenderMathematica(double double1)
+        {
+            return double1.ToString().Replace("E", "*10^");   
+        }
     }
 }
