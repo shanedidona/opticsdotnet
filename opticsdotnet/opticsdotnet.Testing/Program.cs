b97df5f4dc243fe8b6653b34acb807df2feb20b2
Shane DiDona <shanedidona@gmail.com>
2025-02-22 23:24:12 -0500

    [ODN-1] blank ODN1.ODN1.ODN1_1(); and run

--- opticsdotnet/opticsdotnet.Testing/Program.cs
+++ opticsdotnet/opticsdotnet.Testing/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ODN1.ODN1.ODN1_1();
         }
     }
 }
---
+++ opticsdotnet/opticsdotnet.Testing/Tasks/ODN1.cs
@@ -0,0 +1,10 @@
+﻿namespace opticsdotnet.Testing.ODN1
+{
+    public static class ODN1
+    {
+        public static void ODN1_1()
+        {
+
+        }
+    }
+}
