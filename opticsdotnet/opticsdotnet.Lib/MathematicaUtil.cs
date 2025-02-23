31fdccc4ea8bb7eb5e42909bf7782ce8bab081e7
Shane DiDona <shanedidona@gmail.com>
2025-02-22 22:28:12 -0500

    [ODN-1] public static string RenderMathematica(IEnumerable<double> doubles)

--- opticsdotnet/opticsdotnet.Lib/MathematicaUtil.cs
+++ opticsdotnet/opticsdotnet.Lib/MathematicaUtil.cs
@@ -1,10 +1,22 @@
-﻿namespace opticsdotnet.Lib
+﻿using System.Text;
+
+namespace opticsdotnet.Lib
 {
     public static class MathematicaUtil
     {
         public static string RenderMathematica(double double1)
         {
             return double1.ToString().Replace("E", "*10^");   
+        }
+
+        public static string RenderMathematica(IEnumerable<double> doubles)
+        {
+            var sb1 = new StringBuilder();
+            sb1.Append("{");
+            sb1.Append(string.Join(",", doubles.Select(RenderMathematica)));
+            sb1.Append("}");
+
+            return sb1.ToString();
         }
     }
 }
