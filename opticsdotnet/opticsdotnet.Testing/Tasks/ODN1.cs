7b3a3f99e7fc2db748b0a3e791807a58f91eeafe
Shane DiDona <shanedidona@gmail.com>
2025-02-23 16:39:48 -0500

    [ODN-2] change to Circle2D circle2D = new Circle2D(2, 3, 2);

--- opticsdotnet/opticsdotnet.Testing/Tasks/ODN1.cs
+++ opticsdotnet/opticsdotnet.Testing/Tasks/ODN1.cs
@@ -9,7 +9,7 @@ namespace opticsdotnet.Testing.ODN1
             string saveDir = Path.Combine(Program.BaseSaveDir, "ODN-1");
             Directory.CreateDirectory(saveDir);
 
-            Circle2D circle2D = new Circle2D() { Center = new Point2D(2, 3), R = 2 };
+            Circle2D circle2D = new Circle2D(2, 3, 2);
             LineSegment2D lineSegment2D = new LineSegment2D(2, 3, 4, 3.4);
             Point2D point2D = new Point2D(1, 2);
 
