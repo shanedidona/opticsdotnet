1f4790288d24752772cd866658947363e150422d
Shane DiDona <shanedidona@gmail.com>
2025-02-22 23:33:21 -0500

    [ODN-1] load setting

--- opticsdotnet/opticsdotnet.Testing/Program.cs
+++ opticsdotnet/opticsdotnet.Testing/Program.cs
@@ -2,8 +2,12 @@
 {
     internal class Program
     {
+        public static string BaseSaveDir;
+
         static void Main(string[] args)
         {
+            BaseSaveDir = File.ReadAllLines("settings.txt")[0];
+
             ODN1.ODN1.ODN1_1();
         }
     }
