72190aae631749a412b8ed79457bf64f7b2db0f4
Shane DiDona <shanedidona@gmail.com>
2025-02-23 15:41:30 -0500

    [ODN-1] remove methods

--- opticsdotnet/opticsdotnet.Lib/MathematicaUtil.cs
+++ opticsdotnet/opticsdotnet.Lib/MathematicaUtil.cs
@@ -1,40 +1,7 @@
-﻿using System.Text;
-
-namespace opticsdotnet.Lib
+﻿namespace opticsdotnet.Lib
 {
     public static class MathematicaUtil
     {
-        public static string Render(params double[] doubles)
-        {
-            return new MathematicaRenderableMathematicaAdapter(doubles.Select(x => new DoubleMathematicaAdapter(x)).ToArray()).RenderMathematica();
-        }
 
-
-
-
-
-
-
-
-        public static string RenderMathematicaAssumeAlreadyMathematica(params string[] renderedItems)
-        {
-            var sb1 = new StringBuilder();
-            sb1.Append("{");
-            sb1.Append(string.Join(",", renderedItems));
-            sb1.Append("}");
-
-            return sb1.ToString();
-        }
-
-        public static string RenderMathematicaFunction(string name, params string[] args)
-        {
-            var sb1 = new StringBuilder();
-            sb1.Append(name);
-            sb1.Append("[");
-            sb1.Append(string.Join(",", args));
-            sb1.Append("]");
-
-            return sb1.ToString();
-        }
     }
 }
