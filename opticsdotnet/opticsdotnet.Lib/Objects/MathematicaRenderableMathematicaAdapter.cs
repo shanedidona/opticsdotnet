77d0f2895fd1f0f908b2f1413f87e1e938d6ab2e
Shane DiDona <shanedidona@gmail.com>
2025-02-23 13:29:34 -0500

    [ODN-1] public sealed class MathematicaRenderableMathematicaAdapter : IMathematicaRenderable

---
+++ opticsdotnet/opticsdotnet.Lib/Objects/MathematicaRenderableMathematicaAdapter.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace opticsdotnet.Lib
+{
+    public sealed class MathematicaRenderableMathematicaAdapter : IMathematicaRenderable
+    {
+        readonly IMathematicaRenderable[] Array1;
+
+        public MathematicaRenderableMathematicaAdapter(IMathematicaRenderable[] array)
+        {
+            Array1 = array;
+        }
+
+        public string RenderMathematica()
+        {
+            var sb1 = new StringBuilder();
+            sb1.Append("{");
+            sb1.Append(string.Join(",", Array1.Select(x => x.RenderMathematica())));
+            sb1.Append("}");
+
+            return sb1.ToString();
+        }
+    }
+}
