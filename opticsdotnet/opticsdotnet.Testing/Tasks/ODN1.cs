cf135897d2b1f9a3adb99430c1e07ebef1bdb630
Shane DiDona <shanedidona@gmail.com>
2025-02-23 16:21:32 -0500

    [ODN-2] use Point2D.Point2D

--- opticsdotnet/opticsdotnet.Testing/Tasks/ODN1.cs
+++ opticsdotnet/opticsdotnet.Testing/Tasks/ODN1.cs
@@ -9,9 +9,9 @@ namespace opticsdotnet.Testing.ODN1
             string saveDir = Path.Combine(Program.BaseSaveDir, "ODN-1");
             Directory.CreateDirectory(saveDir);
 
-            Circle2D circle2D = new Circle2D() { Center = new Point2D() { X = 2, Y = 3 }, R = 2 };
+            Circle2D circle2D = new Circle2D() { Center = new Point2D(2, 3), R = 2 };
             LineSegment2D lineSegment2D = new LineSegment2D() { X1 = 2, Y1 = 3, X2 = 4, Y2 = 3.4 };
-            Point2D point2D = new Point2D() { X = 1, Y = 2 };
+            Point2D point2D = new Point2D(1, 2);
 
             string string1 = new MathematicaRenderableMathematicaAdapter(
                     new MathematicaRenderableMathematicaAdapter(
