37666cef98b680c546f524dbeaf4864e24f32b37
Shane DiDona <shanedidona@gmail.com>
2025-02-22 21:38:42 -0500

    add X1,Y1,X2,Y2 to LineSegment2D

--- opticsdotnet/opticsdotnet.Lib/Objects/Geo2D/LineSegment2D.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/Geo2D/LineSegment2D.cs
@@ -2,5 +2,9 @@
 {
     public sealed class LineSegment2D : Geo2D
     {
+        public double X1 { get; set; }
+        public double Y1 { get; set; }
+        public double X2 { get; set; }
+        public double Y2 { get; set; }
     }
 }
