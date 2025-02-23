c48f69e15a286f14c4c5c4136e893bb2d6270feb
Shane DiDona <shanedidona@gmail.com>
2025-02-23 13:30:49 -0500

    [ODN-1] copy public string RenderMathematica()

--- opticsdotnet/opticsdotnet.Lib/Objects/MathematicaRenderableMathematicaAdapter.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/MathematicaRenderableMathematicaAdapter.cs
@@ -11,6 +11,23 @@ namespace opticsdotnet.Lib
             Array1 = array;
         }
 
+
+
+
+
+
+
+
+        public string RenderMathematica()
+        {
+            var sb1 = new StringBuilder();
+            sb1.Append("{");
+            sb1.Append(string.Join(",", Array1.Select(x => x.RenderMathematica())));
+            sb1.Append("}");
+
+            return sb1.ToString();
+        }
+
         public string RenderMathematica()
         {
             var sb1 = new StringBuilder();
