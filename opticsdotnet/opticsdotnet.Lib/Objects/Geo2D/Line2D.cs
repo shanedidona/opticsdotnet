e9ff2102a86419b78d48dbc737bf51057c67627d
Shane DiDona <shanedidona@gmail.com>
2025-02-23 19:06:51 -0500

    ODN-5/Line2D (#6)
    * [ODN-5] public sealed class Line2D : Geo2D
    
    * [ODN-5] Line2D line2D = new Line2D(1, 0, 0.1); and use
    
    * [ODN-5] public Line2D ToLine2D()
    
    * [ODN-5] Line2D line2D1 = lineSegment2D.ToLine2D();

---
+++ opticsdotnet/opticsdotnet.Lib/Objects/Geo2D/Line2D.cs
@@ -0,0 +1,30 @@
+﻿namespace opticsdotnet.Lib
+{
+    public sealed class Line2D : Geo2D
+    {
+        public Point2D Point0 { get; set; }
+        public double Theta { get; set; }
+
+
+        public Line2D(double x0, double y0, double theta)
+        {
+            Point0 = new Point2D(x0, y0);
+            Theta = theta;
+        }
+
+
+        public override string RenderMathematica()
+        {
+            return new MathematicaRenderableMathematicaAdapter(
+                    new MathematicaRenderableMathematicaAdapter(
+                                                new DoubleMathematicaAdapter(Point0.X),
+                                                new DoubleMathematicaAdapter(Point0.Y)
+                                            ),
+                    new MathematicaRenderableMathematicaAdapter(
+                                                new DoubleMathematicaAdapter(Math.Cos(Theta)),
+                                                new DoubleMathematicaAdapter(Math.Sin(Theta))
+                                            )
+                ).RenderMathematicaFunction("InfiniteLine");
+        }
+    }
+}
--- opticsdotnet/opticsdotnet.Testing/Tasks/ODN1.cs
+++ opticsdotnet/opticsdotnet.Testing/Tasks/ODN1.cs
@@ -12,14 +12,17 @@ namespace opticsdotnet.Testing.ODN1
             Circle2D circle2D = new Circle2D(2, 3, 2);
             LineSegment2D lineSegment2D = new LineSegment2D(2, 3, 4, 3.4);
             Point2D point2D = new Point2D(1, 2);
+            Line2D line2D = new Line2D(1, 0, 0.1);
 
             string string1 = new MathematicaRenderableMathematicaAdapter(
                     new MathematicaRenderableMathematicaAdapter(
-                            circle2D, lineSegment2D, point2D
+                            circle2D, lineSegment2D, point2D, line2D
                         )
                 ).RenderMathematicaFunction("Graphics");
 
             File.WriteAllText(Path.Combine(saveDir, "ODN1_1.txt"), string1);
+
+            Line2D line2D1 = lineSegment2D.ToLine2D();
         }
     }
 }
