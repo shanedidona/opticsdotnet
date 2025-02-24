3e4afe05fd9e64682ca16af5cdacad91ebd2457f
Shane DiDona <shanedidona@gmail.com>
2025-02-24 18:14:16 -0500

    ODN-7/Distance-Between-Line2D-and-Point (#8)
    * [ODN-7] ODN-7.wl
    
    * [ODN-7] blank public sealed class Line2DPoint2DDistanceResult
    
    * [ODN-7] partial
    
    * [ODN-7] update ODN-7.wl
    
    * [ODN-7] public sealed class Line2DPoint2DDistanceResult
    
    * [ODN-7] public Line2DPoint2DDistanceResult LinePointDistanceCalc(double x0, double y0, double theta, double xp, double yp)
    
    * [ODN-7] static

---
+++ opticsdotnet/opticsdotnet.Lib/Objects/Line2DPoint2DDistanceResult.cs
@@ -0,0 +1,9 @@
+﻿namespace opticsdotnet.Lib
+{
+    public sealed class Line2DPoint2DDistanceResult
+    {
+        public double AMin;
+        public double DMin;
+        public Point2D ClosestPoint;
+    }
+}
