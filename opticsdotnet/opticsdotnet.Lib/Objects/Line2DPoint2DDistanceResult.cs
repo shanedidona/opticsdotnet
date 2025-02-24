2cb780f676240ecc2ccf0189162c3ee0fcca62fc
Shane DiDona <shanedidona@gmail.com>
2025-02-23 20:53:18 -0500

    [ODN-7] public sealed class Line2DPoint2DDistanceResult

--- opticsdotnet/opticsdotnet.Lib/Objects/Line2DPoint2DDistanceResult.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/Line2DPoint2DDistanceResult.cs
@@ -1,6 +1,9 @@
-﻿namespace opticsdotnet.Lib.Objects
+﻿namespace opticsdotnet.Lib
 {
     public sealed class Line2DPoint2DDistanceResult
     {
+        public double AMin;
+        public double DMin;
+        public Point2D ClosestPoint;
     }
 }
