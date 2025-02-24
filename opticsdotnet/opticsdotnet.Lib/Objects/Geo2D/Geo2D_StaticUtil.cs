02b32e7acfb31a5b0e1c1520738c4344529150f3
Shane DiDona <shanedidona@gmail.com>
2025-02-23 20:53:33 -0500

    [ODN-7] public Line2DPoint2DDistanceResult LinePointDistanceCalc(double x0, double y0, double theta, double xp, double yp)

---
+++ opticsdotnet/opticsdotnet.Lib/Objects/Geo2D/Geo2D_StaticUtil.cs
@@ -0,0 +1,24 @@
+﻿namespace opticsdotnet.Lib
+{
+    public abstract partial class Geo2D : IMathematicaRenderable
+    {
+        public Line2DPoint2DDistanceResult LinePointDistanceCalc(double x0, double y0, double theta, double xp, double yp)
+        {
+            double aMin = (-x0 + xp) * Math.Cos(theta) + (-y0 + yp) * Math.Sin(theta);
+
+            double dMin = Math.Abs((y0 - yp) * Math.Cos(theta) + (-x0 + xp) * Math.Sin(theta));
+
+            Point2D closestPoint = new Point2D(
+                        x0 + aMin * Math.Cos(theta),
+                        y0 + aMin * Math.Sin(theta)
+                    );
+
+            return new Line2DPoint2DDistanceResult()
+            {
+                AMin = aMin,
+                DMin = dMin,
+                ClosestPoint = closestPoint
+            };
+        }
+    }
+}
