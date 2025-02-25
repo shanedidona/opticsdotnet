4a293254578ae548d86b4e0e2baf5c6123a0aa30
Shane DiDona <shanedidona@gmail.com>
2025-02-24 20:51:28 -0500

    [ODN-13] process code

--- opticsdotnet/opticsdotnet.Lib/Objects/Geo2D/Circle2D.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/Geo2D/Circle2D.cs
@@ -1,15 +1,19 @@
 ﻿namespace opticsdotnet.Lib
 {
-    public sealed class Circle2D : Geo2D
+    public sealed class Arc2D : Geo2D
     {
         public Point2D Center { get; set; }
         public double R { get; set; }
+        public double Theta1 { get; set; }
+        public double Theta2 { get; set; }
 
 
-        public Circle2D(double xC, double yC, double r)
+        public Arc2D(double xC, double yC, double r, double theta1, double theta2)
         {
             Center = new Point2D(xC, yC);
             R = r;
+            Theta1 = theta1;
+            Theta2 = theta2;
         }
 
 
