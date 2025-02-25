624b65a7d95c7e5e72f511e777608cf78b1c2493
Shane DiDona <shanedidona@gmail.com>
2025-02-24 20:26:28 -0500

    [ODN-13] Circle2D - Copy.cs

---
+++ opticsdotnet/opticsdotnet.Lib/Objects/Geo2D/Circle2D - Copy.cs
@@ -0,0 +1,27 @@
+﻿namespace opticsdotnet.Lib
+{
+    public sealed class Circle2D : Geo2D
+    {
+        public Point2D Center { get; set; }
+        public double R { get; set; }
+
+
+        public Circle2D(double xC, double yC, double r)
+        {
+            Center = new Point2D(xC, yC);
+            R = r;
+        }
+
+
+        public override string RenderMathematica()
+        {
+            return new MathematicaRenderableMathematicaAdapter(
+                        new MathematicaRenderableMathematicaAdapter(
+                                new DoubleMathematicaAdapter(Center.X),
+                                new DoubleMathematicaAdapter(Center.Y)
+                            ),
+                            new DoubleMathematicaAdapter(R)
+                        ).RenderMathematicaFunction("Circle");
+        }
+    }
+}
