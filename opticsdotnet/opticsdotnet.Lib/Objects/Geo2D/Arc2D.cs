f3aa2d9a9a3c4a8911e7e2319d30cf247681b554
Shane DiDona <shanedidona@gmail.com>
2025-02-24 20:54:16 -0500

    [ODN-13] switch files?

--- opticsdotnet/opticsdotnet.Lib/Objects/Geo2D/Arc2D.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/Geo2D/Arc2D.cs
@@ -1,25 +1,29 @@
 ﻿namespace opticsdotnet.Lib
 {
-    public sealed class Circle2D : Geo2D
+    public sealed class Arc2D : Geo2D
     {
         public Point2D Center { get; set; }
         public double R { get; set; }
+        public double Theta1 { get; set; }
+        public double Theta2 { get; set; }
 
 
-        public Circle2D(double xC, double yC, double r)
+        public Arc2D(double xC, double yC, double r, double theta1, double theta2)
         {
             Center = new Point2D(xC, yC);
             R = r;
+            Theta1 = theta1;
+            Theta2 = theta2;
         }
 
 
         public override string RenderMathematica()
         {
             return new MathematicaRenderableMathematicaAdapter(
-                        new MathematicaRenderableMathematicaAdapter(
-                                new DoubleMathematicaAdapter(Center.X),
-                                new DoubleMathematicaAdapter(Center.Y)
-                            ),
+                            new MathematicaRenderableMathematicaAdapter(
+                                    new DoubleMathematicaAdapter(Center.X),
+                                    new DoubleMathematicaAdapter(Center.Y)
+                                ),
                             new DoubleMathematicaAdapter(R)
                         ).RenderMathematicaFunction("Circle");
         }
