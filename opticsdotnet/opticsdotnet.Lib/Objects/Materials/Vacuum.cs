f0e45795554a70ed30e0b65c0fa61a6730682069
Shane DiDona <shanedidona@gmail.com>
2025-02-26 20:19:38 -0500

    [ODN-9] public sealed class Vacuum : IOpticalMaterial

---
+++ opticsdotnet/opticsdotnet.Lib/Objects/Materials/Vacuum.cs
@@ -0,0 +1,15 @@
+﻿namespace opticsdotnet.Lib
+{
+    public sealed class Vacuum : IOpticalMaterial
+    {
+        public double? AbsorptionCoefficient(double wavelength)
+        {
+            return 0;
+        }
+
+        public double? IndexOfRefraction(double wavelength)
+        {
+            return 1;
+        }
+    }
+}
