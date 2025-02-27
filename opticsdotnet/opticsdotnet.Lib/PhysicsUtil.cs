4a3ed0891b13f402f4e5111bfb22d592a70d2bb8
Shane DiDona <shanedidona@gmail.com>
2025-02-26 21:11:27 -0500

    [ODN-9] public static double SellmeierEvaluate(

--- opticsdotnet/opticsdotnet.Lib/PhysicsUtil.cs
+++ opticsdotnet/opticsdotnet.Lib/PhysicsUtil.cs
@@ -2,5 +2,25 @@
 {
     public static class PhysicsUtil
     {
+        public static double SellmeierEvaluate(
+                double wavelength,//nanometers
+                double b1,
+                double b2,
+                double b3,
+                double c1,
+                double c2,
+                double c3
+            )
+        {
+            double lambdaSquared = 0.001 * wavelength;
+            lambdaSquared = lambdaSquared * lambdaSquared;
+
+            double nSquared = 1;
+            nSquared += b1 * lambdaSquared / (lambdaSquared - c1);
+            nSquared += b2 * lambdaSquared / (lambdaSquared - c2);
+            nSquared += b3 * lambdaSquared / (lambdaSquared - c3);
+
+            return Math.Sqrt(nSquared);
+        }
     }
 }
