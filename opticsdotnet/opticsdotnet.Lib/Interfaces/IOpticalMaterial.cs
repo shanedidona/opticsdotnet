3011d19726da0a2f0de190fa3e33431b18cd7e8f
Shane DiDona <shanedidona@gmail.com>
2025-02-26 20:11:42 -0500

    [ODN-9] public interface IOpticalMaterial

---
+++ opticsdotnet/opticsdotnet.Lib/Interfaces/IOpticalMaterial.cs
@@ -0,0 +1,8 @@
+﻿namespace opticsdotnet.Lib.Interfaces
+{
+    public interface IOpticalMaterial
+    {
+        double? IndexOfRefraction(double wavelength);//In Nanometers
+        double? AbsorptionCoefficient(double wavelength);//Returns 1/m, accepts nanometers
+    }
+}
