190b03e3b5d3522a066bdd9cc7725282c9137cac
Shane DiDona <shanedidona@gmail.com>
2025-02-26 21:31:07 -0500

    [ODN-9] index of refraction of NBK7 (and citation)

--- opticsdotnet/opticsdotnet.Lib/Objects/Materials/NBK7.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/Materials/NBK7.cs
@@ -2,9 +2,28 @@
 {
     public sealed class NBK7 : IOpticalMaterial
     {
+        //https://www.thorlabs.com/newgrouppage9.cfm?objectgroup_id=6973&tabname=N-BK7
+        const double B1 = 1.03961212;
+        const double B2 = 0.231792344;
+        const double B3 = 1.01046945;
+        const double C1 = 0.00600069867;
+        const double C2 = 0.0200179144;
+        const double C3 = 103.560653;
+
+
         public double? AbsorptionCoefficient(double wavelength)
         {
-            
+            if (wavelength < 300)
+            {
+                return null;
+            }
+
+            if (2500 < wavelength)
+            {
+                return null;
+            }
+
+            return PhysicsUtil.SellmeierEvaluate(wavelength, B1, B2, B3, C1, C2, C3);
         }
 
         public double? IndexOfRefraction(double wavelength)
