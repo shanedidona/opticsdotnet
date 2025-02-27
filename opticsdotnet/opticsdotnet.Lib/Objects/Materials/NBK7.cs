74b7388cc4151898b57030632bb2e5c48f1d01a6
Shane DiDona <shanedidona@gmail.com>
2025-02-26 21:12:40 -0500

    [ODN-7] incomplete NBK7.cs

---
+++ opticsdotnet/opticsdotnet.Lib/Objects/Materials/NBK7.cs
@@ -0,0 +1,15 @@
+﻿namespace opticsdotnet.Lib
+{
+    public sealed class NBK7 : IOpticalMaterial
+    {
+        public double? AbsorptionCoefficient(double wavelength)
+        {
+            
+        }
+
+        public double? IndexOfRefraction(double wavelength)
+        {
+            
+        }
+    }
+}
