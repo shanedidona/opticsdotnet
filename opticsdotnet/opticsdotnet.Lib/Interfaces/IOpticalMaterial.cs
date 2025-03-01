8922b4ec9fc4beda463d641e4213265b50765d31
Shane DiDona <shanedidona@gmail.com>
2025-03-01 18:28:23 -0500

    [ODN-9] change to double? AbsorptionCoefficient(double wavelength);//Returns 1/m (where 1 atten for 1 meter results in e^-1 transmission), accepts nanometers

--- opticsdotnet/opticsdotnet.Lib/Interfaces/IOpticalMaterial.cs
+++ opticsdotnet/opticsdotnet.Lib/Interfaces/IOpticalMaterial.cs
@@ -3,6 +3,6 @@
     public interface IOpticalMaterial
     {
         double? IndexOfRefraction(double wavelength);//In Nanometers
-        double? AbsorptionCoefficient(double wavelength);//Returns 1/m, accepts nanometers
+        double? AbsorptionCoefficient(double wavelength);//Returns 1/m (where 1 atten for 1 meter results in e^-1 transmission), accepts nanometers
     }
 }
