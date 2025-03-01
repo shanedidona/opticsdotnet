5871b72c036b48e81677308a78fc9552c6d508cf
Shane DiDona <shanedidona@gmail.com>
2025-03-01 17:34:08 -0500

    [ODN-9] public static MathNet.Numerics.Interpolation.LinearSpline LinearSplineFromCSVFile(string path)

--- opticsdotnet/opticsdotnet.Lib/IOUtil.cs
+++ opticsdotnet/opticsdotnet.Lib/IOUtil.cs
@@ -45,5 +45,17 @@
 
             return (xs, ys);
         }
+
+        public static MathNet.Numerics.Interpolation.LinearSpline LinearSplineFromCSVFile(string path)
+        {
+            MathNet.Numerics.LinearAlgebra.Matrix<double> mat1 = MathNet.Numerics.Data.Text.DelimitedReader.Read<double>(path, false, ",", false);
+
+            //int numRows = mat1.RowCount;
+
+            double[] xs = mat1.Column(0).ToArray();
+            double[] ys = mat1.Column(1).ToArray();
+
+            return MathNet.Numerics.Interpolation.LinearSpline.InterpolateSorted(xs, ys);
+        }
     }
 }
