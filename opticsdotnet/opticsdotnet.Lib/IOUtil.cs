6b99c640078bba56fcc6fc1c0ec57af3b1652e1f
Shane DiDona <shanedidona@gmail.com>
2025-03-01 21:14:24 -0500

    [ODN-9] remove 2 methods

--- opticsdotnet/opticsdotnet.Lib/IOUtil.cs
+++ opticsdotnet/opticsdotnet.Lib/IOUtil.cs
@@ -2,23 +2,6 @@
 {
     public static class IOUtil
     {
-        public static string[][] ParseCSV(string path)//TODO: replace with a library for edge cases
-        {
-            var out1 = new List<string[]>();
-
-            foreach (string line in File.ReadAllLines(path))
-            {
-                out1.Add(ParseCSVLine(line));
-            }
-
-            return out1.ToArray();
-        }
-
-        static string[] ParseCSVLine(string line)//TODO: replace with a library for edge cases
-        {
-            return line.Split(",").Select(x => x.Trim('"')).ToArray();
-        }
-
         static (double[] Xs, double?[] Ys) ParsePhysicalData(string[][] splitLines)
         {
             double[] xs = new double[splitLines.Length];
