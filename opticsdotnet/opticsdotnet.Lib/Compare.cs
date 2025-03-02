5452c0ea09051382b4543c785b29e0383804da72
Shane DiDona <shanedidona@gmail.com>
2025-03-01 21:11:54 -0500

    [ODN-9] public static bool ValidateIncreasing<T>(T[] array) where T : IComparable<T>

--- opticsdotnet/opticsdotnet.Lib/Compare.cs
+++ opticsdotnet/opticsdotnet.Lib/Compare.cs
@@ -2,5 +2,19 @@
 {
     public static class Compare
     {
+        public static bool ValidateIncreasing<T>(T[] array) where T : IComparable<T>
+        {
+            if (array.Length < 2) { return true; }
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (0 <= array[i - 1].CompareTo(array[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
