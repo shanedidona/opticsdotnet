c2a6c484db9f3d4fb13d0bfa86268a93ae00d3ea
Shane DiDona <shanedidona@gmail.com>
2025-03-02 11:10:44 -0500

    [ODN-11] incomplete public sealed class AxiLens : IMathematicaRenderable

---
+++ opticsdotnet/opticsdotnet.Lib/Objects/Elements/AxiLens.cs
@@ -0,0 +1,35 @@
+﻿namespace opticsdotnet.Lib
+{
+    public sealed class AxiLens : IMathematicaRenderable
+    {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        readonly double Length1;
+        readonly IOpticalMaterial OpticalMaterial;
+
+        public AxiDrift(IOpticalMaterial opticalMaterial, double length)
+        {
+            OpticalMaterial = opticalMaterial;
+            Length1 = length;
+        }
+
+        public string RenderMathematica()
+        {
+            return MathematicaUtil.Nothing;
+        }
+    }
+}
