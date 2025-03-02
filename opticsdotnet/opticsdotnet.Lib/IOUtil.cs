2ae192c69558e5ed8327c753d346e48a3b98fbec
Shane DiDona <shanedidona@gmail.com>
2025-03-01 19:12:26 -0500

    [ODN-9] process code

--- opticsdotnet/opticsdotnet.Lib/IOUtil.cs
+++ opticsdotnet/opticsdotnet.Lib/IOUtil.cs
@@ -2,6 +2,17 @@
 {
     public static class IOUtil
     {
+
+
+
+
+
+
+
+
+
+
+
         public static string[][] ParseCSV(string path)//TODO: replace with a library for edge cases
         {
             var out1 = new List<string[]>();
