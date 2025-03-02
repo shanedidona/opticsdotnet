700a744e4894b899df39dcf7f283b5766883a940
Shane DiDona <shanedidona@gmail.com>
2025-03-01 21:31:42 -0500

    [ODN-9] skeleton testing file

---
+++ opticsdotnet/opticsdotnet.Testing/Tasks/ODN9.cs
@@ -0,0 +1,19 @@
+﻿using opticsdotnet.Lib;
+
+namespace opticsdotnet.Testing.ODN9
+{
+    public static class ODN9
+    {
+        public static void ODN9_1()
+        {
+            string saveDir = Path.Combine(Program.BaseSaveDir, "ODN-9");
+            Directory.CreateDirectory(saveDir);
+
+
+
+
+
+
+        }
+    }
+}
