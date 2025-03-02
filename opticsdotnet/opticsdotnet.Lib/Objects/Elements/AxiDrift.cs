27e2ff2b50423584aa3674e655bbec79f9db8a1e
Shane DiDona <shanedidona@gmail.com>
2025-03-02 10:24:00 -0500

    [ODN-16] public sealed class AxiDrift

---
+++ opticsdotnet/opticsdotnet.Lib/Objects/Elements/AxiDrift.cs
@@ -0,0 +1,12 @@
+﻿namespace opticsdotnet.Lib
+{
+    public sealed class AxiDrift
+    {
+        readonly double Length1;
+
+        public AxiDrift(double length)
+        {
+            Length1 = length;
+        }
+    }
+}
