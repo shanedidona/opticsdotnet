82b0de9330ffbe001dd9d8c0eb463abf773ff8ed
Shane DiDona <shanedidona@gmail.com>
2025-03-02 10:27:50 -0500

    [ODN-16] change to public sealed class AxiDrift : IMathematicaRenderable

--- opticsdotnet/opticsdotnet.Lib/Objects/Elements/AxiDrift.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/Elements/AxiDrift.cs
@@ -1,12 +1,17 @@
 ﻿namespace opticsdotnet.Lib
 {
-    public sealed class AxiDrift
+    public sealed class AxiDrift : IMathematicaRenderable
     {
         readonly double Length1;
 
         public AxiDrift(double length)
         {
             Length1 = length;
+        }
+
+        public string RenderMathematica()
+        {
+            return MathematicaUtil.Nothing;
         }
     }
 }
