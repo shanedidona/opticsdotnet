0bae6176b58f1c48ae9922896b17929a674f5891
Shane DiDona <shanedidona@gmail.com>
2025-03-02 20:10:56 -0500

    [ODN-18] public static class MathUtil and public static double Sq(double value)

---
+++ opticsdotnet/opticsdotnet.Lib/MathUtil.cs
@@ -0,0 +1,10 @@
+﻿namespace opticsdotnet.Lib
+{
+    public static class MathUtil
+    {
+        public static double Sq(double value)
+        {
+            return value * value;
+        }
+    }
+}
