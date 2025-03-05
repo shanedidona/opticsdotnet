f67a2be71579c8c80e7670152b1be5e7ac213076
Shane DiDona <shanedidona@gmail.com>
2025-03-04 19:18:30 -0500

    [ODN-11] add some code to public string RenderMathematica()

--- opticsdotnet/opticsdotnet.Lib/Objects/Elements/AxiLens.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/Elements/AxiLens.cs
@@ -20,25 +20,34 @@
         public string RenderMathematica()
         {
 
+            Line2D lowerEdge = new Line2D(0, -OuterRadius, 0);
+            Line2D upperEdge = new Line2D(0, OuterRadius, 0);
 
+            IMathematicaRenderable leftSurface;
+            if (Radius1.HasValue)
+            {
+                Circle2D circle = new Circle2D(-Radius1.Value, 0, Radius1.Value);
 
+                leftSurface = circle;
+            }
+            else
+            {
+                leftSurface = new LineSegment2D(0, -OuterRadius, 0, OuterRadius);
+            }
 
+            IMathematicaRenderable rightSurface;
+            if (Radius2.HasValue)
+            {
+                Circle2D circle = new Circle2D(CenterThickness + Radius2.Value, 0, Radius2.Value);
 
+                rightSurface = circle;
+            }
+            else
+            {
+                rightSurface = new LineSegment2D(CenterThickness, -OuterRadius, 0, OuterRadius);
+            }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-            return MathematicaUtil.Nothing;
+            return new MathematicaRenderableMathematicaAdapter(lowerEdge, upperEdge, leftSurface, rightSurface).RenderMathematica();
         }
     }
 }
