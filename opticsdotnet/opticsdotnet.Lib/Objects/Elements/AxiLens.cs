7203c07aa6a5bf4b1117658a9db7c60e1b415b84
Shane DiDona <shanedidona@gmail.com>
2025-03-04 19:18:02 -0500

    [ODN-11] add comments

--- opticsdotnet/opticsdotnet.Lib/Objects/Elements/AxiLens.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/Elements/AxiLens.cs
@@ -4,8 +4,8 @@
     {
         readonly double CenterThickness;
         readonly double OuterRadius;
-        readonly double? Radius1;
-        readonly double? Radius2;
+        readonly double? Radius1;//Null means flat
+        readonly double? Radius2;//Null means flat
         readonly IOpticalMaterial OpticalMaterial;
 
         public AxiLens(IOpticalMaterial opticalMaterial, double centerThickness, double outerRadius, double? radius1, double? radius2)
