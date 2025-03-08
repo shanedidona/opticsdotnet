135ac20f75dfc78ca6ebbec5df0d8a4b9f30d2d5
Shane DiDona <shanedidona@gmail.com>
2025-03-08 18:49:19 -0500

    [ODN-23] public sealed class AxiRayState

---
+++ opticsdotnet/opticsdotnet.Lib/Objects/AxiRayState.cs
@@ -0,0 +1,20 @@
+﻿namespace opticsdotnet.Lib
+{
+    public sealed class AxiRayState
+    {
+        public readonly double Z0;
+        public readonly double R0;
+        public readonly double Theta;
+        public readonly double WaveLength;
+        public readonly double Intensity;
+
+        public AxiRayState(double z0, double r0, double theta, double waveLength, double intensity)
+        {
+            Z0 = z0;
+            R0 = r0;
+            Theta = theta;
+            WaveLength = waveLength;
+            Intensity = intensity;
+        }
+    }
+}
