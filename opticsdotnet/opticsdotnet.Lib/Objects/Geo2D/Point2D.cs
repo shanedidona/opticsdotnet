cde83cb9733991012545280c0680cc6f005c20c8
Shane DiDona <shanedidona@gmail.com>
2025-03-08 17:18:41 -0500

    ODN-11/AxiLens (#20)
    * [ODN-11] incomplete public sealed class AxiLens : IMathematicaRenderable
    
    * [ODN-11] add some code to public sealed class AxiLens : IMathematicaRenderable
    
    * [ODN-11] start on testing
    
    * [ODN-11] add comments
    
    * [ODN-11] add some code to public string RenderMathematica()
    
    * [ODN-11] testing and display
    
    * [ODN-11] add citations
    
    * [ODN-11] add to comments
    
    * [ODN-11] correct comments
    
    * [ODN-11] add to code
    
    * [ODN-11] testing
    
    * [ODN-11] change center thickness
    
    * [ODN-11] testing changes
    
    * [ODN-11] cleanup testing
    
    * [ODN-11] public static class Point2D_Extensions
    
    * [ODN-11] upper and lower points for front and back circles
    
    * [ODN-11] also render diag objects
    
    * [ODN-11] add 3 more lenses
    
    * [ODN-11] change to      if (0 < Radius2.Value)
    
    * [ODN-11] use abs of radii for circle generation radii
    
    * [ODN-11] render origin
    
    * [ODN-11] 2x    Math.Abs
    
    * [ODN-11] make origin a diag object to render
    
    * [ODN-11] add citation
    
    * [ODN-11] public static Point2D operator +(Point2D point1, Point2D point2) and minus
    
    * [ODN-11] public Arc2D GetArc(double theta1, double theta2)
    
    * [ODN-11] public double R() and public double Theta()
    
    * [ODN-11] handle case of convex left surface
    
    * [ODN-11] handle concave left surface
    
    * [ODN-11] convex right surface
    
    * [ODN-11] concave right surface
    
    * [AVNF-11] 4x rename
    
    * [ODN-11] 4x rename
    
    * [ODN-11] add another lens
    
    * [ODN-11] correct to rightSurface = new LineSegment2D(CenterThickness, -OuterRadius, CenterThickness, OuterRadius);
    
    * [ODN-11] public LineSegment2D(Point2D point1, Point2D point2)
    
    * [ODN-11] change to leftSurface = new LineSegment2D(lowerLeftPoint, upperLeftPoint);
    
    * [ODN-11] rightSurface = new LineSegment2D(lowerRightPoint, upperRightPoint);
    
    * [ODN-11] Point2D lowerLeftPoint; and upper
    
    * [ODN-11] Point2D lowerRightPoint; and upper
    
    * [ODN-11] 4x add to diag objects
    
    * [ODN-11] collect lines
    
    * [ODN-11] collect lines
    
    * [ODN-11] move lines
    
    * [ODN-11] move line
    
    * [ODN-11] bottom surface and top surface
    
    * [ODN-11] comment out objectsToRender.AddRange(diagObjects);
    
    * [ODN-11] remove blank line
    
    * [ODN-11] commit testing output

--- opticsdotnet/opticsdotnet.Lib/Objects/Geo2D/Point2D.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/Geo2D/Point2D.cs
@@ -22,5 +22,48 @@
                                 )
                     ).RenderMathematicaFunction("Point");
         }
+
+        public double R()
+        {
+            return Math.Sqrt(X * X + Y * Y);
+        }
+
+        public double Theta()
+        {
+            return Math.Atan2(Y, X);
+        }
+
+        public static Point2D operator +(Point2D point1, Point2D point2)
+        {
+            return new Point2D(point1.X + point2.X, point1.Y + point2.Y);
+        }
+
+        public static Point2D operator -(Point2D point1, Point2D point2)
+        {
+            return new Point2D(point1.X - point2.X, point1.Y - point2.Y);
+        }
+    }
+
+    public static class Point2D_Extensions
+    {
+        public static Point2D TopMost(this Point2D[] points)
+        {
+            return points.OrderBy(point => point.Y).Last();
+        }
+
+        public static Point2D BottomMost(this Point2D[] points)
+        {
+            return points.OrderBy(point => point.Y).First();
+        }
+
+        public static Point2D LeftMost(this Point2D[] points)
+        {
+            return points.OrderBy(point => point.X).First();
+        }
+
+        public static Point2D RightMost(this Point2D[] points)
+        {
+            return points.OrderBy(point => point.X).Last();
+        }
     }
 }
