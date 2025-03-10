c19a0170247bf1862c3eca45d71647eb91a2a5ae
Shane DiDona <shanedidona@gmail.com>
2025-03-10 13:44:21 -0400

    [ODN-22] use => instead

--- opticsdotnet/opticsdotnet.Lib/Vendors/Thorlabs/NBK7PlanoConvexLensesUncoated.cs
+++ opticsdotnet/opticsdotnet.Lib/Vendors/Thorlabs/NBK7PlanoConvexLensesUncoated.cs
@@ -5,10 +5,7 @@
         //https://www.thorlabs.com/newgrouppage9.cfm?objectgroup_id=112
 
 
-        public static AxiLens LA1024()
-        {
-            return new AxiLens(new NBK7(), 0.001, 0.001, 0.0021, null);
-        }
+        public static AxiLens LA1024() => new AxiLens(new NBK7(), 0.001, 0.001, 0.0021, null);
 
     }
 }
