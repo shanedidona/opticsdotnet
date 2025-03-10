e819d026c454b78d8a0dd956b4834b43033b0fdb
Shane DiDona <shanedidona@gmail.com>
2025-03-10 14:11:18 -0400

    [ODN-22] 6mm diameter

--- opticsdotnet/opticsdotnet.Lib/Vendors/Thorlabs/NBK7PlanoConvexLensesUncoated.cs
+++ opticsdotnet/opticsdotnet.Lib/Vendors/Thorlabs/NBK7PlanoConvexLensesUncoated.cs
@@ -10,5 +10,10 @@
 
         public static AxiLens LA1036() => new AxiLens(new NBK7(), 0.0015, 0.0015, 0.0031, null);
         public static AxiLens LA1039() => new AxiLens(new NBK7(), 0.0015, 0.0015, 0.0047, null);
+
+        public static AxiLens LA1116() => new AxiLens(new NBK7(), 0.0025, 0.003, 0.0052, null);
+        public static AxiLens LA1470() => new AxiLens(new NBK7(), 0.0023, 0.003, 0.0062, null);
+        public static AxiLens LA1222() => new AxiLens(new NBK7(), 0.0021, 0.003, 0.0077, null);
+        public static AxiLens LA1700() => new AxiLens(new NBK7(), 0.0018, 0.003, 0.0155, null);
     }
 }
