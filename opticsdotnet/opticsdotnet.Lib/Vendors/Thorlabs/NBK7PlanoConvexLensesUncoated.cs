7f11b9802eeef577e1aba50136c05d59c962e67f
Shane DiDona <shanedidona@gmail.com>
2025-03-10 13:55:02 -0400

    [ODN-22] LA1026

--- opticsdotnet/opticsdotnet.Lib/Vendors/Thorlabs/NBK7PlanoConvexLensesUncoated.cs
+++ opticsdotnet/opticsdotnet.Lib/Vendors/Thorlabs/NBK7PlanoConvexLensesUncoated.cs
@@ -6,6 +6,6 @@
 
 
         public static AxiLens LA1024() => new AxiLens(new NBK7(), 0.001, 0.001, 0.0021, null);
-
+        public static AxiLens LA1026() => new AxiLens(new NBK7(), 0.001, 0.001, 0.0031, null);
     }
 }
