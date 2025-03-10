fbeb8cfa9c49be400bd43aa30db38283cf3ec626
Shane DiDona <shanedidona@gmail.com>
2025-03-10 12:14:54 -0400

    [ODN-24] add code to public sealed class AxiRay

--- opticsdotnet/opticsdotnet.Lib/Objects/AxiRay.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/AxiRay.cs
@@ -2,11 +2,21 @@
 {
     public sealed class AxiRay
     {
+        readonly List<AxiRayState> States;
 
+        public AxiRay(AxiRayState initialState)
+        {
+            States = new List<AxiRayState>() { initialState };
+        }
 
+        public AxiRayState GetCurrentState()
+        {
+            return States.Last();
+        }
 
-
-
-
+        public void AddRange(params AxiRayState[] statesToAdd)
+        {
+            States.AddRange(statesToAdd);
+        }
     }
 }
