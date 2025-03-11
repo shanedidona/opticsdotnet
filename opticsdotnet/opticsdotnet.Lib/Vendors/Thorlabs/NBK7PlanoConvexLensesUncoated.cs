ba9e63fd3d13297d9c3016a37c2b97dd02f3023a
Shane DiDona <shanedidona@gmail.com>
2025-03-11 15:11:19 -0400

    [ODN-22] correct 9mm lenses

--- opticsdotnet/opticsdotnet.Lib/Vendors/Thorlabs/NBK7PlanoConvexLensesUncoated.cs
+++ opticsdotnet/opticsdotnet.Lib/Vendors/Thorlabs/NBK7PlanoConvexLensesUncoated.cs
@@ -16,8 +16,8 @@
         public static AxiLens LA1222() => new AxiLens(new NBK7(), 0.0021, 0.003, 0.0077, null);
         public static AxiLens LA1700() => new AxiLens(new NBK7(), 0.0018, 0.003, 0.0155, null);
 
-        public static AxiLens LA1576() => new AxiLens(new NBK7(), 0.034, 0.045, 0.0062, null);
-        public static AxiLens LA1472() => new AxiLens(new NBK7(), 0.025, 0.045, 0.0103, null);
+        public static AxiLens LA1576() => new AxiLens(new NBK7(), 0.0034, 0.0045, 0.0062, null);
+        public static AxiLens LA1472() => new AxiLens(new NBK7(), 0.0025, 0.0045, 0.0103, null);
 
         public static AxiLens LA1540() => new AxiLens(new NBK7(), 0.0051, 0.0127 / 2, 0.0077, null);
         public static AxiLens LA1074() => new AxiLens(new NBK7(), 0.0040, 0.0127 / 2, 0.0103, null);
