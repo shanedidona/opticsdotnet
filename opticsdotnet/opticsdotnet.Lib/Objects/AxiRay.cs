8456e66d03f0ab6a2798f41d657a2d5d574c9166
Shane DiDona <shanedidona@gmail.com>
2025-03-11 18:15:56 -0400

    ODN-24/AxiRay (#26)
    * [ODN-24] blank               public sealed class AxiRay
    
    * [ODN-24] add code to public sealed class AxiRay
    
    * [ODN-24] change to public sealed class AxiRay : IMathematicaRenderable

---
+++ opticsdotnet/opticsdotnet.Lib/Objects/AxiRay.cs
@@ -0,0 +1,31 @@
+﻿namespace opticsdotnet.Lib
+{
+    public sealed class AxiRay : IMathematicaRenderable
+    {
+        readonly List<AxiRayState> States;
+
+        public AxiRay(AxiRayState initialState)
+        {
+            States = new List<AxiRayState>() { initialState };
+        }
+
+        public AxiRayState GetCurrentState()
+        {
+            return States.Last();
+        }
+
+        public void AddRange(params AxiRayState[] statesToAdd)
+        {
+            States.AddRange(statesToAdd);
+        }
+
+        public string RenderMathematica()
+        {
+            return new PolyLine2D(
+                                        States.Select(
+                                            state => new Point2D(state.Z0, state.R0)
+                                        ).ToArray()
+                                    ).RenderMathematica();
+        }
+    }
+}
