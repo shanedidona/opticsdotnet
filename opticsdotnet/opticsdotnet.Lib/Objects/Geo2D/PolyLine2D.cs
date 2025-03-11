fd3e53951ea841847c03cc08f73f05ebd09e52fb
Shane DiDona <shanedidona@gmail.com>
2025-03-11 17:34:56 -0400

    [ODN-31] public sealed class PolyLine2D : Geo2D

---
+++ opticsdotnet/opticsdotnet.Lib/Objects/Geo2D/PolyLine2D.cs
@@ -0,0 +1,38 @@
+﻿namespace opticsdotnet.Lib
+{
+    public sealed class PolyLine2D : Geo2D
+    {
+        readonly double[] Xs;
+        readonly double[] Ys;
+
+
+        public PolyLine2D(params Point2D[] points)
+        {
+            Xs = new double[points.Length];
+            Ys = new double[points.Length];
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                Xs[i] = points[i].X;
+                Ys[i] = points[i].Y;
+            }
+        }
+
+        public override string RenderMathematica()
+        {
+
+            var innerPart = new MathematicaRenderableMathematicaAdapter[Xs.Length];
+            for (int i = 0; i < Xs.Length; i++)
+            {
+                innerPart[i] = new MathematicaRenderableMathematicaAdapter(
+                                                new DoubleMathematicaAdapter(Xs[i]),
+                                                new DoubleMathematicaAdapter(Ys[i])
+                                            );
+            }
+
+            return new MathematicaRenderableMathematicaAdapter(
+                        new MathematicaRenderableMathematicaAdapter(innerPart)
+                        ).RenderMathematicaFunction("Line");
+        }
+    }
+}
