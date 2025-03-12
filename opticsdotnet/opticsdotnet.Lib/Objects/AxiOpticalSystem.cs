54827046919cde3102dac05dae25e2e3d8e59d91
Shane DiDona <shanedidona@gmail.com>
2025-03-12 11:25:38 -0400

    [ODN-36] incomplete public sealed class AxiOpticalSystem : IMathematicaRenderable

---
+++ opticsdotnet/opticsdotnet.Lib/Objects/AxiOpticalSystem.cs
@@ -0,0 +1,19 @@
+﻿namespace opticsdotnet.Lib.Objects
+{
+    public sealed class AxiOpticalSystem : IMathematicaRenderable
+    {
+        readonly IAxiRaySource AxiRaySource;
+        readonly AxiDrift[] Drifts;
+        readonly IAxiOpticalElement[] Elements;
+        readonly IAxiRayTerminator RayTerminator;
+
+
+
+
+
+        public string RenderMathematica()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
