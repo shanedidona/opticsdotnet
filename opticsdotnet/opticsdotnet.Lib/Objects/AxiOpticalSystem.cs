5b845db19ee78f5cb334f0e5261083299f9a5f17
Shane DiDona <shanedidona@gmail.com>
2025-03-12 17:27:00 -0400

    [ODN-36] public AxiOpticalSystem(

--- opticsdotnet/opticsdotnet.Lib/Objects/AxiOpticalSystem.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/AxiOpticalSystem.cs
@@ -7,9 +7,28 @@
         readonly IAxiOpticalElement[] AxiElements;
         readonly IAxiRayTerminator AxiRayTerminator;
 
+        public AxiOpticalSystem(
+                IAxiRaySource axiRaySource,
+                AxiDrift[] axiDrifts,
+                IAxiOpticalElement[] axiElements,
+                IAxiRayTerminator axiRayTerminator
+            )
+        {
+            AxiRaySource = axiRaySource;
+            AxiDrifts = axiDrifts;
+            AxiElements = axiElements;
+            AxiRayTerminator = axiRayTerminator;
 
+            if (axiDrifts.Length < 1)
+            {
+                throw new NotSupportedException("There must be at least one IAxiDrift");
+            }
 
-
+            if (axiDrifts.Length != (axiElements.Length + 1))
+            {
+                throw new NotSupportedException("axiDrifts.Length != (axiElements.Length + 1)");
+            }
+        }
 
         public string RenderMathematica()
         {
