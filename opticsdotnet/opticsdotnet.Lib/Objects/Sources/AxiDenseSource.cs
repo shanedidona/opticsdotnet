928af0486789c2fbd431d36677a91837ecb7f95e
Shane DiDona <shanedidona@gmail.com>
2025-03-12 09:57:13 -0400

    ODN-34/IAxiRaySource (#37)
    * [ODN-34] add citation
    
    * [ODN-34] public interface IAxiRaySource : IMathematicaRenderable
    
    * [ODN-34] public sealed class AxiDenseSource : IAxiRaySource

---
+++ opticsdotnet/opticsdotnet.Lib/Objects/Sources/AxiDenseSource.cs
@@ -0,0 +1,50 @@
+﻿namespace opticsdotnet.Lib
+{
+    public sealed class AxiDenseSource : IAxiRaySource
+    {
+        readonly IEnumerable<double> Z0s;
+        readonly IEnumerable<double> R0s;
+        readonly IEnumerable<double> Thetas;
+        readonly IEnumerable<double> WaveLengths;
+        readonly double? Intensity;
+
+        public AxiDenseSource(
+                IEnumerable<double> z0s,
+                IEnumerable<double> r0s,
+                IEnumerable<double> thetas,
+                IEnumerable<double> waveLengths,
+                double? intensity
+            )
+        {
+            Z0s = z0s;
+            R0s = r0s;
+            Thetas = thetas;
+            WaveLengths = waveLengths;
+            Intensity = intensity;
+        }
+
+        public IEnumerable<AxiRay> AxiRays()
+        {
+            foreach (double z0 in Z0s)
+            {
+                foreach (double r0 in R0s)
+                {
+                    foreach (double theta in Thetas)
+                    {
+                        foreach (double waveLength in WaveLengths)
+                        {
+                            yield return new AxiRay(
+                                    new AxiRayState(z0, r0, theta, waveLength, Intensity)
+                                );
+                        }
+                    }
+                }
+            }
+        }
+
+        public string RenderMathematica()
+        {
+            return MathematicaUtil.Nothing;
+        }
+    }
+}
