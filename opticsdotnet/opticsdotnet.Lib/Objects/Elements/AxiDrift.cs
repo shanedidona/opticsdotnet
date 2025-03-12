0bc467f07f8d67b43ea683c60538976dac7a98e8
Shane DiDona <shanedidona@gmail.com>
2025-03-12 11:27:39 -0400

    [ODN-39] 2x public

--- opticsdotnet/opticsdotnet.Lib/Objects/Elements/AxiDrift.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/Elements/AxiDrift.cs
@@ -2,8 +2,8 @@
 {
     public sealed class AxiDrift
     {
-        readonly double Length1;
-        readonly IOpticalMaterial OpticalMaterial;
+        public readonly double Length1;
+        public readonly IOpticalMaterial OpticalMaterial;
 
         public AxiDrift(IOpticalMaterial opticalMaterial, double length)
         {
