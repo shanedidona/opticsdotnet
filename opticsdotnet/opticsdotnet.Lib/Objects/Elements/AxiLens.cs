16c05591805c2971acd00caaf393169c4263bd5a
Shane DiDona <shanedidona@gmail.com>
2025-03-18 21:01:12 -0400

    [ODN-44] formatting

--- opticsdotnet/opticsdotnet.Lib/Objects/Elements/AxiLens.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/Elements/AxiLens.cs
@@ -26,15 +26,9 @@ namespace opticsdotnet.Lib
             {
                 AxiRayState currentState = axiRay.GetCurrentState();
 
-                if (!currentState.Theta.HasValue)
-                {
-                    continue;
-                }
+                if (!currentState.Theta.HasValue) { continue; }
 
-                if (!currentState.Intensity.HasValue)
-                {
-                    continue;
-                }
+                if (!currentState.Intensity.HasValue) { continue; }
 
                 Line2D line2DRelToThis = new Line2D(currentState.Z0 - thisZ0, currentState.R0, currentState.Theta.Value);
 
