a82478bfa1964292655d067ee816fd6587075567
Shane DiDona <shanedidona@gmail.com>
2025-03-20 21:57:24 -0400

    [ODN-44] incomplete static void AxiRayTraceCircle(

--- opticsdotnet/opticsdotnet.Lib/Objects/Elements/AxiLens/AxiLens_UtilityStatic.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/Elements/AxiLens/AxiLens_UtilityStatic.cs
@@ -2,6 +2,32 @@
 {
     public sealed partial class AxiLens : IAxiOpticalElement
     {
-        
+        static void AxiRayTraceCircle(
+                AxiRay axiRay,
+                double? nLeft,
+                double? nRight,
+                Circle2D circle,
+                bool openingRight,//For left surface this means convex, for right surface this means concave
+                double circleIntersectionWithAxis,//The intersection that the circle makes with the axis for the part that we care about
+                double outerRadius
+            )
+        {
+            AxiRayState currentState = axiRay.GetCurrentState();
+
+            if (!currentState.Theta.HasValue) { continue; }
+
+            if (!currentState.Intensity.HasValue) { continue; }
+
+
+
+
+
+
+
+
+
+
+
+        }
     }
 }
