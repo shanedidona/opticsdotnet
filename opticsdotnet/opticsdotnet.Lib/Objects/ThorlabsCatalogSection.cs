b7f18581096efc0b5030ccb7ca7f8984b2206b32
Shane DiDona <shanedidona@gmail.com>
2025-05-02 23:12:35 -0400

    [ODN-52] public sealed class ThorlabsCatalogSection

---
+++ opticsdotnet/opticsdotnet.Lib/Objects/ThorlabsCatalogSection.cs
@@ -0,0 +1,14 @@
+﻿namespace opticsdotnet.Lib.Objects
+{
+    public sealed class ThorlabsCatalogSection
+    {
+        public string Name;
+        public IAxiOpticalElement[] AxiOpticalElements;
+
+        public ThorlabsCatalogSection(string name, IAxiOpticalElement[] axiOpticalElements)
+        {
+            Name = name;
+            AxiOpticalElements = axiOpticalElements;
+        }
+    }
+}
