46a807d52aff62eac56adfca4aa1bf900706da07
Shane DiDona <shanedidona@gmail.com>
2025-05-03 22:39:50 -0400

    [ODN-54] commit

--- opticsdotnet/opticsdotnet.Lib/MathematicaUtil.cs
+++ opticsdotnet/opticsdotnet.Lib/MathematicaUtil.cs
@@ -3,5 +3,25 @@
     public static class MathematicaUtil
     {
         public const string Nothing = "Nothing";
+
+        public static void woooo<TKey,TValue>() where TKey : IMathematicaRenderable, TValue where TValue: IMathematicaRenderable
+        {
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
     }
 }
