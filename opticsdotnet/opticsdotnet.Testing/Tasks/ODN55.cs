c967aa6175616450f4a8451bf5e4cb98e29b7a47
Shane DiDona <shanedidona@gmail.com>
2025-05-05 20:58:04 -0400

    ODN-55/StringMathematicaAdapter (#56)
    * [ODN-54] add citation
    
    * [ODN-55] public sealed class StringMathematicaAdapter : IMathematicaRenderable
    
    * [ODN-55] test code

---
+++ opticsdotnet/opticsdotnet.Testing/Tasks/ODN55.cs
@@ -0,0 +1,27 @@
+﻿using opticsdotnet.Lib;
+
+namespace opticsdotnet.Testing.ODN55
+{
+    public static class ODN55
+    {
+        public static void ODN55_1()
+        {
+            string saveDir = Path.Combine(Program.BaseSaveDir, "ODN-55");
+            Directory.CreateDirectory(saveDir);
+
+            string[] stringsIn = new string[]
+            {
+                "test1",
+                "\ntest1",
+                "\\",
+                ""
+
+            };
+
+            string[] stringsOut = stringsIn.Select(x => (new StringMathematicaAdapter(x)).RenderMathematica()).ToArray();
+
+            File.WriteAllLines(Path.Combine(saveDir, "stringsIn.txt"), stringsIn);
+            File.WriteAllLines(Path.Combine(saveDir, "stringsOut.txt"), stringsOut);
+        }
+    }
+}
