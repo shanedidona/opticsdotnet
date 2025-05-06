a4a65b38dd8e9aa0bf82ef38f3b82ab30ab0e73d
Shane DiDona <shanedidona@gmail.com>
2025-05-05 21:57:58 -0400

    [ODN-54] commit

---
+++ Tasks/ODN-54/ODN-54.wl
@@ -0,0 +1,12 @@
+(* ::Package:: *)
+
+var1=Association[];
+InputForm[var1]
+
+
+var1["a"]="b";
+InputForm[var1]
+
+
+var1["a1"]="b1";
+InputForm[var1]
--- opticsdotnet/opticsdotnet.Lib/MathematicaUtil.cs
+++ opticsdotnet/opticsdotnet.Lib/MathematicaUtil.cs
@@ -1,15 +1,25 @@
-﻿namespace opticsdotnet.Lib
+﻿using System.Text;
+
+namespace opticsdotnet.Lib
 {
     public static class MathematicaUtil
     {
         public const string Nothing = "Nothing";
 
-        public static void woooo<TKey,TValue>() where TKey : IMathematicaRenderable, TValue where TValue: IMathematicaRenderable
+        public static void woooo<TKey, TValue>(Dictionary<TKey, TValue> dict) where TKey : IMathematicaRenderable, TValue where TValue : IMathematicaRenderable
         {
+            StringBuilder sb1 = new StringBuilder();
 
+            KeyValuePair<TKey,TValue>[] keyValuePairs = dict.ToArray();
 
+            sb1.Append("<|");
 
+            foreach (KeyValuePair<TKey, TValue> keyValuePair in keyValuePairs)
+            {
 
+            }
+
+            sb1.Append("|>");
 
 
 
