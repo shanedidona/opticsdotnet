16071d852505806a19cbda0f64ce995b24bf2e82
Shane DiDona <shanedidona@gmail.com>
2025-05-10 12:53:02 -0400

    [ODN-44] process code

--- opticsdotnet/opticsdotnet.Testing/Tasks/ODN36.cs
+++ opticsdotnet/opticsdotnet.Testing/Tasks/ODN36.cs
@@ -115,45 +115,6 @@ namespace opticsdotnet.Testing.ODN36
                                 );
             }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            var axiLenses = new List<AxiLens>();
-            var expectedFocalZRelToBacks = new List<double>();
-
-            axiLenses.Add(opticsdotnet.Lib.Vendors.Thorlabs.Catalog.LA1859());
-            expectedFocalZRelToBacks.Add(0.0153);
-
-            var focalPointRelToBacks = new List<Point2D>();
-            foreach (AxiLens axiLens in axiLenses)
-            {
-                focalPointRelToBacks.Add(
-                            Analysis.CalculateFocalPointRelToBack(
-                            beamRadius,
-                            numRays,
-                            wavelengthNM,
-                            frontDriftLength,
-                            backDriftLength,
-                            axiLens
-                        )
-                    );
-            }
-
             double[] fracZErrors = Enumerable.Range(0, axiLenses.Count)
                 .Select(
                             i => (focalPointRelToBacks[i].X - expectedFocalZRelToBacks[i]) / expectedFocalZRelToBacks[i]
