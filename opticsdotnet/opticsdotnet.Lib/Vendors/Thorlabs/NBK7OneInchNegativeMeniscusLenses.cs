e95f4aee4490e64bf0729bb3d76b6597c777c969
Shane DiDona <shanedidona@gmail.com>
2025-05-17 22:54:06 -0400

    [ODN-62] NBK7OneInchNegativeMeniscusLenses.cs

---
+++ opticsdotnet/opticsdotnet.Lib/Vendors/Thorlabs/NBK7OneInchNegativeMeniscusLenses.cs
@@ -0,0 +1,12 @@
+﻿namespace opticsdotnet.Lib.Vendors.Thorlabs
+{
+    public static partial class Catalog
+    {
+        //https://www.thorlabs.com/newgrouppage9.cfm?objectgroup_id=902
+        //Note that this is just the uncoated lenses on the page
+
+
+
+
+    }
+}
