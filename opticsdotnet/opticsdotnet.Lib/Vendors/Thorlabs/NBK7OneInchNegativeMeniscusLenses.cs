9295ad5d224ca99a06a6c733954b2898f505e7ca
Shane DiDona <shanedidona@gmail.com>
2025-05-18 12:51:25 -0400

    [ODN-62] add some code

--- opticsdotnet/opticsdotnet.Lib/Vendors/Thorlabs/NBK7OneInchNegativeMeniscusLenses.cs
+++ opticsdotnet/opticsdotnet.Lib/Vendors/Thorlabs/NBK7OneInchNegativeMeniscusLenses.cs
@@ -6,6 +6,18 @@
         //Note that this is just the uncoated lenses on the page
 
 
+        public static AxiLens LF1822() => new AxiLens(new NBK7(), 0.003, 0.0254 / 2, 0.100, -0.0337);
+        public static AxiLens LF1547() => new AxiLens(new NBK7(), 0.003, 0.0254 / 2, 0.100, -0.0431);
+        public static AxiLens LF1097() => new AxiLens(new NBK7(), 0.003, 0.0254 / 2, 0.100, -0.0502);
+        public static AxiLens LF1015() => new AxiLens(new NBK7(), 0.003, 0.0254 / 2, 0.250, -0.0951);
+        public static AxiLens LF1544() => new AxiLens(new NBK7(), 0.003, 0.0254 / 2, 0.250, -0.1125);
+        public static AxiLens LF1988() => new AxiLens(new NBK7(), 0.003, 0.0254 / 2, 0.250, -0.1263);
+        public static AxiLens LF1141() => new AxiLens(new NBK7(), 0.003, 0.0254 / 2, 0.500, -0.2532);
+
+
+
+
+
 
 
     }
