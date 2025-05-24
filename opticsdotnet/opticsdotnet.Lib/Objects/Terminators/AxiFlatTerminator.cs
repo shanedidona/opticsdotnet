9b047307f3b48bac065c21203e6f7438e14d2744
Shane DiDona <shanedidona@gmail.com>
2025-05-23 20:57:59 -0400

    [ODN-35] interface and method

--- opticsdotnet/opticsdotnet.Lib/Objects/Terminators/AxiFlatTerminator.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/Terminators/AxiFlatTerminator.cs
@@ -2,6 +2,11 @@
 {
     public sealed class AxiFlatTerminator : IAxiRayTerminator
     {
+        public void AxiRayTrace(double thisZ0, AxiDrift previousDrift, AxiRay[] axiRays)
+        {
+            throw new NotImplementedException();
+        }
+
         public string RenderMathematica()
         {
             return (new Line2D(0, 0, Math.PI / 2)).RenderMathematica();
