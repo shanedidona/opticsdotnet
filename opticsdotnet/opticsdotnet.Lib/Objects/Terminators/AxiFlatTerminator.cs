56c2927ca7e5352f80cd53a75d87045ef84d4b3d
Shane DiDona <shanedidona@gmail.com>
2025-05-23 22:24:00 -0400

    [ODN-35] Line2D Flat = new Line2D(0, 0, PiOver2); and use

--- opticsdotnet/opticsdotnet.Lib/Objects/Terminators/AxiFlatTerminator.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/Terminators/AxiFlatTerminator.cs
@@ -4,14 +4,44 @@ namespace opticsdotnet.Lib
 {
     public sealed class AxiFlatTerminator : IAxiRayTerminator
     {
+        Line2D Flat = new Line2D(0, 0, PiOver2);
+
+
         public void AxiRayTrace(double thisZ0, AxiDrift previousDrift, AxiRay[] axiRays)
         {
+            foreach (AxiRay axiRay in axiRays)
+            {
+                AxiRayState currentState = axiRay.GetCurrentState();
+
+                if (!currentState.Theta.HasValue) { return; }
+
+                if (!currentState.Intensity.HasValue) { return; }
+
+                //Relative to the point we care about where this flat intersects the axis
+                Line2D incomingLine = new Line2D(currentState.Z0 - thisZ0, currentState.R0, currentState.Theta.Value);
+
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
             throw new NotImplementedException();
         }
 
         public string RenderMathematica()
         {
-            return (new Line2D(0, 0, PiOver2)).RenderMathematica();
+            return Flat.RenderMathematica();
         }
     }
 }
