a07bcaa10648b89723a93f91fd8bc98034ac98b6
Shane DiDona <shanedidona@gmail.com>
2025-05-24 20:18:35 -0400

    [ODN-35] update optical system test

--- opticsdotnet/opticsdotnet.Testing/Tasks/ODN36.cs
+++ opticsdotnet/opticsdotnet.Testing/Tasks/ODN36.cs
@@ -12,7 +12,7 @@ namespace opticsdotnet.Testing.ODN36
             var axiDenseSource = new AxiDenseSource(
                                             new double[] { -0.001, 0 },
                                             new double[] { -0.001, 0, 0.001 },
-                                            new double[] { -0.4, -0.05, 0, 0.05, 0.4 },
+                                            new double[] { -0.2, -0.05, 0, 0.05, 0.2 },
                                             new double[] { 500, 600, 700 },
                                             1.0
                                         );
@@ -27,7 +27,7 @@ namespace opticsdotnet.Testing.ODN36
 
             var axiElements = new IAxiOpticalElement[]
             {
-                opticsdotnet.Lib.Vendors.Thorlabs.Catalog.LA1859(),
+                opticsdotnet.Lib.Vendors.Thorlabs.Catalog.LBF254_040(),
                 opticsdotnet.Lib.Vendors.Thorlabs.Catalog.LA1255(),
                 opticsdotnet.Lib.Vendors.Thorlabs.Catalog.LA1859().ReturnFlipped()
             };
