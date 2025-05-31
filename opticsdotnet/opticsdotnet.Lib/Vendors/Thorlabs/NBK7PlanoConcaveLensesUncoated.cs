64f1005ce8c616581ee26d9c1717f0f5fa9d1b9a
Shane DiDona <shanedidona@gmail.com>
2025-05-30 22:12:18 -0400

    [ODN-27] replace

--- opticsdotnet/opticsdotnet.Lib/Vendors/Thorlabs/NBK7PlanoConcaveLensesUncoated.cs
+++ opticsdotnet/opticsdotnet.Lib/Vendors/Thorlabs/NBK7PlanoConcaveLensesUncoated.cs
@@ -5,25 +5,25 @@
         //https://www.thorlabs.com/newgrouppage9.cfm?objectgroup_id=2087
 
 
-        public static AxiLens LC1975() => new AxiLens(new NBK7(), 0.002, 0.003, -0.0124, null);
+        public static SphericalSinglet LC1975() => new SphericalSinglet(new NBK7(), 0.002, 0.003, -0.0124, null);
 
-        public static AxiLens LC1906() => new AxiLens(new NBK7(), 0.002, 0.0045, -0.0139, null);
+        public static SphericalSinglet LC1906() => new SphericalSinglet(new NBK7(), 0.002, 0.0045, -0.0139, null);
 
-        public static AxiLens LC1054() => new AxiLens(new NBK7(), 0.003, 0.0127 / 2, -0.0129, null);
-        public static AxiLens LC1060() => new AxiLens(new NBK7(), 0.003, 0.0127 / 2, -0.0154, null);
-        public static AxiLens LC1439() => new AxiLens(new NBK7(), 0.0035, 0.0127 / 2, -0.0257, null);
+        public static SphericalSinglet LC1054() => new SphericalSinglet(new NBK7(), 0.003, 0.0127 / 2, -0.0129, null);
+        public static SphericalSinglet LC1060() => new SphericalSinglet(new NBK7(), 0.003, 0.0127 / 2, -0.0154, null);
+        public static SphericalSinglet LC1439() => new SphericalSinglet(new NBK7(), 0.0035, 0.0127 / 2, -0.0257, null);
 
-        public static AxiLens LC1259() => new AxiLens(new NBK7(), 0.0035, 0.0125, -0.0257, null);
-        public static AxiLens LC1258() => new AxiLens(new NBK7(), 0.0035, 0.0125, -0.0386, null);
-        public static AxiLens LC1254() => new AxiLens(new NBK7(), 0.004, 0.0125, -0.0515, null);
+        public static SphericalSinglet LC1259() => new SphericalSinglet(new NBK7(), 0.0035, 0.0125, -0.0257, null);
+        public static SphericalSinglet LC1258() => new SphericalSinglet(new NBK7(), 0.0035, 0.0125, -0.0386, null);
+        public static SphericalSinglet LC1254() => new SphericalSinglet(new NBK7(), 0.004, 0.0125, -0.0515, null);
 
-        public static AxiLens LC1715() => new AxiLens(new NBK7(), 0.0035, 0.0254 / 2, -0.0257, null);
-        public static AxiLens LC1582() => new AxiLens(new NBK7(), 0.0035, 0.0254 / 2, -0.0386, null);
-        public static AxiLens LC1120() => new AxiLens(new NBK7(), 0.004, 0.0254 / 2, -0.0515, null);
+        public static SphericalSinglet LC1715() => new SphericalSinglet(new NBK7(), 0.0035, 0.0254 / 2, -0.0257, null);
+        public static SphericalSinglet LC1582() => new SphericalSinglet(new NBK7(), 0.0035, 0.0254 / 2, -0.0386, null);
+        public static SphericalSinglet LC1120() => new SphericalSinglet(new NBK7(), 0.004, 0.0254 / 2, -0.0515, null);
 
-        public static AxiLens LC1315() => new AxiLens(new NBK7(), 0.0035, 0.0508 / 2, -0.0386, null);
-        public static AxiLens LC1093() => new AxiLens(new NBK7(), 0.004, 0.0508 / 2, -0.0515, null);
-        public static AxiLens LC1611() => new AxiLens(new NBK7(), 0.004, 0.0508 / 2, -0.0772, null);
+        public static SphericalSinglet LC1315() => new SphericalSinglet(new NBK7(), 0.0035, 0.0508 / 2, -0.0386, null);
+        public static SphericalSinglet LC1093() => new SphericalSinglet(new NBK7(), 0.004, 0.0508 / 2, -0.0515, null);
+        public static SphericalSinglet LC1611() => new SphericalSinglet(new NBK7(), 0.004, 0.0508 / 2, -0.0772, null);
 
         public static ThorlabsCatalogPage NBK7PlanoConcaveLensesUncoated()
         {
