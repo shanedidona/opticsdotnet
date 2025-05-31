0d54ceae590198008c75b93b2880e2e5aedb045e
Shane DiDona <shanedidona@gmail.com>
2025-05-31 12:26:37 -0400

    [ODN-72] starter public sealed class NSF11 : IOpticalMaterial

---
+++ opticsdotnet/opticsdotnet.Lib/Objects/Materials/NSF11.cs
@@ -0,0 +1,23 @@
+﻿namespace opticsdotnet.Lib
+{
+    public sealed class NSF11 : IOpticalMaterial
+    {
+        public double? IndexOfRefractionMinWavelength => throw new NotImplementedException();
+
+        public double? IndexOfRefractionMaxWavelength => throw new NotImplementedException();
+
+        public double? AbsorptionCoefficientMinWavelength => throw new NotImplementedException();
+
+        public double? AbsorptionCoefficientMaxWavelength => throw new NotImplementedException();
+
+        public double? AbsorptionCoefficient(double wavelength)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double? IndexOfRefraction(double wavelength)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
