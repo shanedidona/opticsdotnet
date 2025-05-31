ff371ce653767853bef37a2d0ffb952126c58fe8
Shane DiDona <shanedidona@gmail.com>
2025-05-31 12:28:37 -0400

    [ODN-72] add citations

--- opticsdotnet/opticsdotnet.Lib/Objects/Materials/NSF11.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/Materials/NSF11.cs
@@ -2,6 +2,10 @@
 {
     public sealed class NSF11 : IOpticalMaterial
     {
+        //https://www.thorlabs.com/newgrouppage9.cfm?objectgroup_id=6973&tabname=N-SF11
+        //https://www.thorlabs.com/images/TabImages/Uncoated_N-SF11_Transmission.xlsx
+
+
         public double? IndexOfRefractionMinWavelength => throw new NotImplementedException();
 
         public double? IndexOfRefractionMaxWavelength => throw new NotImplementedException();
