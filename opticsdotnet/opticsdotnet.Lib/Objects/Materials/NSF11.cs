835aeb6b94e8e2fb60992d9f45bc5283dce4104b
Shane DiDona <shanedidona@gmail.com>
2025-06-01 17:08:39 -0400

    [ODN-72] fill in more for index of refraction

--- opticsdotnet/opticsdotnet.Lib/Objects/Materials/NSF11.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/Materials/NSF11.cs
@@ -5,12 +5,18 @@
         //https://www.thorlabs.com/newgrouppage9.cfm?objectgroup_id=6973&tabname=N-SF11
         //https://www.thorlabs.com/images/TabImages/Uncoated_N-SF11_Transmission.xlsx
 
+        const double B1 = 1.73759695;
+        const double B2 = 0.313747346;
+        const double B3 = 1.89878101;
+        const double C1 = 0.013188707;
+        const double C2 = 0.0623068142;
+        const double C3 = 155.23629;
 
         static readonly LinearSplineWithMinMax AbsorptionCoefficientLinearSplineWithMinMax = IOUtil.LinearSplineWithMinMaxFromCSVFile("Data//NSF11//NFS11_AttenuationCoeffs.csv");
 
-        public double? IndexOfRefractionMinWavelength => throw new NotImplementedException();
+        public double? IndexOfRefractionMinWavelength => 370;
 
-        public double? IndexOfRefractionMaxWavelength => throw new NotImplementedException();
+        public double? IndexOfRefractionMaxWavelength => 2500;
 
         public double? AbsorptionCoefficientMinWavelength => AbsorptionCoefficientLinearSplineWithMinMax.MinIndep;
 
