d4153cddad186ea133431386562ebb127ba32ff7
Shane DiDona <shanedidona@gmail.com>
2025-06-01 21:29:52 -0400

    [ODN-72] PhysicsUtil.OpticalMaterialPlot(new NSF11(), Path.Combine(saveDir, "NSF11.png"), 1000);

--- opticsdotnet/opticsdotnet.Testing/Tasks/OpticalMaterials.cs
+++ opticsdotnet/opticsdotnet.Testing/Tasks/OpticalMaterials.cs
@@ -10,6 +10,7 @@ namespace opticsdotnet.Testing.OpticalMaterials
             Directory.CreateDirectory(saveDir);
 
             PhysicsUtil.OpticalMaterialPlot(new NBK7(), Path.Combine(saveDir, "NBK7.png"), 1000);
+            PhysicsUtil.OpticalMaterialPlot(new NSF11(), Path.Combine(saveDir, "NSF11.png"), 1000);
         }
     }
 }
