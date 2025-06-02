30107865310a5f3527a316b495dfeeb2b0e0ab03
Shane DiDona <shanedidona@gmail.com>
2025-06-01 21:56:20 -0400

    [ODN-73] NBK7andNSF11BiConcaveLensesUncoated.cs

---
+++ opticsdotnet/opticsdotnet.Lib/Vendors/Thorlabs/NBK7andNSF11BiConcaveLensesUncoated.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opticsdotnet.Lib.Vendors.Thorlabs
+{
+    class NBK7andNSF11BiConcaveLensesUncoated
+    {
+    }
+}
