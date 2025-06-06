c0658ad6aec2efe3b738ba1dc1fa04136a3facb0
Shane DiDona <shanedidona@gmail.com>
2025-06-05 22:14:03 -0400

    [ODN-78] commit

--- opticsdotnet/opticsdotnet.Lib/Objects/AxiOpticalAssembly.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/AxiOpticalAssembly.cs
@@ -1,6 +1,38 @@
 ﻿namespace opticsdotnet.Lib
 {
-    public sealed class AxiOpticalAssembly
+    public sealed class AxiOpticalAssembly : IMathematicaRenderable
     {
+        readonly AxiDrift[] AxiDrifts;
+        readonly IAxiOpticalElement[] AxiElements;
+
+        readonly int NumOpticalElements;
+        readonly double[] AxiElementOffsets;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public string RenderMathematica()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
