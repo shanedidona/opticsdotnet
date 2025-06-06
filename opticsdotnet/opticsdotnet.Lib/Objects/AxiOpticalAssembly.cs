acd612d72303fba10efeb568db684d1a1c009235
Shane DiDona <shanedidona@gmail.com>
2025-06-05 22:16:54 -0400

    [ODN-78] copy code

--- opticsdotnet/opticsdotnet.Lib/Objects/AxiOpticalAssembly.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/AxiOpticalAssembly.cs
@@ -9,7 +9,45 @@
         readonly double[] AxiElementOffsets;
 
 
+        public AxiOpticalSystem(
+                IAxiRaySource axiRaySource,
+                AxiDrift[] axiDrifts,
+                IAxiOpticalElement[] axiElements,
+                IAxiRayTerminator axiRayTerminator
+            )
+        {
+            AxiRaySource = axiRaySource;
+            AxiDrifts = axiDrifts;
+            AxiElements = axiElements;
+            AxiRayTerminator = axiRayTerminator;
 
+            if (axiDrifts.Length < 1)
+            {
+                throw new NotSupportedException("There must be at least one IAxiDrift");
+            }
+
+            if (axiDrifts.Length != (axiElements.Length + 1))
+            {
+                throw new NotSupportedException("axiDrifts.Length != (axiElements.Length + 1)");
+            }
+
+            NumOpticalElements = AxiElements.Length;
+            AxiElementOffsets = new double[NumOpticalElements];
+
+            for (int i = 0; i < NumOpticalElements; i++)
+            {
+                if (i == 0)
+                {
+                    AxiElementOffsets[0] = AxiDrifts[0].Length1;
+                }
+                else
+                {
+                    AxiElementOffsets[i] = AxiElementOffsets[i - 1] + AxiElements[i - 1].CenterLength + AxiDrifts[i].Length1;
+                }
+            }
+
+            AxiRayTerminatorOffset = AxiElementOffsets.Last() + AxiDrifts.Last().Length1;
+        }
 
 
 
