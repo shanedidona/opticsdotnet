20b09035502bcc82624c7e8741c445f855a73758
Shane DiDona <shanedidona@gmail.com>
2025-06-07 18:33:07 -0400

    [ODN-78] process code

--- opticsdotnet/opticsdotnet.Lib/Objects/AxiOpticalSystem.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/AxiOpticalSystem.cs
@@ -32,17 +32,7 @@
             
 
 
-            for (int i = 0; i < NumOpticalElements; i++)
-            {
-                if (i == 0)
-                {
-                    AxiElementOffsets[0] = AxiDrifts[0].Length1;
-                }
-                else
-                {
-                    AxiElementOffsets[i] = AxiElementOffsets[i - 1] + AxiElements[i - 1].CenterLength + AxiDrifts[i].Length1;
-                }
-            }
+            
 
             AxiRayTerminatorOffset = AxiElementOffsets.Last() + AxiDrifts.Last().Length1;
         }
