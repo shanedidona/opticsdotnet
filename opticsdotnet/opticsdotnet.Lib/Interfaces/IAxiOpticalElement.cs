32620e5b8a047ae3e46358f11ad0c94244aa1b0a
Shane DiDona <shanedidona@gmail.com>
2025-06-08 16:19:44 -0400

    [ODN-81] change to public AxiRay AxiRayTraceSingleRay(double thisZ0, AxiDrift previousDrift, AxiDrift nextDrift, AxiRay axiRay);//TODO: support total internal reflection

--- opticsdotnet/opticsdotnet.Lib/Interfaces/IAxiOpticalElement.cs
+++ opticsdotnet/opticsdotnet.Lib/Interfaces/IAxiOpticalElement.cs
@@ -6,6 +6,6 @@
 
         public IEnumerable<AxiRay> AxiRayTrace(double thisZ0, AxiDrift previousDrift, AxiDrift nextDrift, IEnumerable<AxiRay> axiRays);//TODO: support total internal reflection
 
-        public IEnumerable<AxiRay> AxiRayTrace(double thisZ0, AxiDrift previousDrift, AxiDrift nextDrift, IEnumerable<AxiRay> axiRays);//TODO: support total internal reflection
+        public AxiRay AxiRayTraceSingleRay(double thisZ0, AxiDrift previousDrift, AxiDrift nextDrift, AxiRay axiRay);//TODO: support total internal reflection
     }
 }
