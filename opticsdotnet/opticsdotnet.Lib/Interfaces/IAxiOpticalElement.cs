6dce96b4d83a1219e23a3c31813351fb2bcfafb8
Shane DiDona <shanedidona@gmail.com>
2025-06-08 16:08:31 -0400

    [ODN-81] copy line

--- opticsdotnet/opticsdotnet.Lib/Interfaces/IAxiOpticalElement.cs
+++ opticsdotnet/opticsdotnet.Lib/Interfaces/IAxiOpticalElement.cs
@@ -5,5 +5,7 @@
         public double CenterLength { get; }
 
         public IEnumerable<AxiRay> AxiRayTrace(double thisZ0, AxiDrift previousDrift, AxiDrift nextDrift, IEnumerable<AxiRay> axiRays);//TODO: support total internal reflection
+
+        public IEnumerable<AxiRay> AxiRayTrace(double thisZ0, AxiDrift previousDrift, AxiDrift nextDrift, IEnumerable<AxiRay> axiRays);//TODO: support total internal reflection
     }
 }
