0cb4f58878015ede02888602506f3fde203f8571
Shane DiDona <shanedidona@gmail.com>
2025-06-15 23:21:43 -0400

    [ODN-84] public sealed class RGBColor : MathematicaDirective

---
+++ opticsdotnet/opticsdotnet.Lib/Mathematica/Objects/MathematicaDirective/RGBColor.cs
@@ -0,0 +1,28 @@
+﻿namespace opticsdotnet.Lib.Mathematica
+{
+    public sealed class RGBColor : MathematicaDirective
+    {
+        readonly double R;
+        readonly double G;
+        readonly double B;
+        readonly double A;
+
+        public RGBColor(double r, double g, double b, double a = 1.0)
+        {
+            R = r;
+            G = g;
+            B = b;
+            A = a;
+        }
+
+        public override string RenderMathematica()
+        {
+            return (new MathematicaRenderableMathematicaAdapter(
+                    new DoubleMathematicaAdapter(R),
+                    new DoubleMathematicaAdapter(G),
+                    new DoubleMathematicaAdapter(B),
+                    new DoubleMathematicaAdapter(A)
+                )).RenderMathematicaFunction("RGBColor");
+        }
+    }
+}
