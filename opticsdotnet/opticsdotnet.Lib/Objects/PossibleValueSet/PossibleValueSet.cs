b4d5d0784a3027a8b9fb34ecb93ba554a828fb1a
Shane DiDona <shanedidona@gmail.com>
2025-06-21 18:09:54 -0400

    [ODN-90] break apart into public abstract class PossibleValueSet<T> : PossibleValueSet

--- opticsdotnet/opticsdotnet.Lib/Objects/PossibleValueSet/PossibleValueSet.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/PossibleValueSet/PossibleValueSet.cs
@@ -2,18 +2,36 @@
 
 namespace opticsdotnet.Lib
 {
-    public abstract class PossibleValueSet<T>
+    public abstract class PossibleValueSet
     {
         public abstract int? NumItems { get; }//Null means infinity
+    }
+
+    public abstract class PossibleValueSet<T> : PossibleValueSet
+    {
         public abstract T FirstItem { get; }
         public abstract T LastItem { get; }
     }
 
     public static class PossibleValueSet_Extensions
     {
-        public static BigInteger woooo<T>(this PossibleValueSet<T> sets)
+        public static BigInteger? NumberOfPermutations(this PossibleValueSet[] possibleValueSets)
         {
-            return 1;
+            BigInteger out1 = 1;
+
+            foreach (PossibleValueSet possibleValueSet in possibleValueSets)
+            {
+                if (possibleValueSet.NumItems.HasValue)
+                {
+                    out1 *= possibleValueSet.NumItems.Value;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            return out1;
         }
     }
 }
