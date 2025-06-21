23246d40668f8cc16151dc646f37a6f039526fee
Shane DiDona <shanedidona@gmail.com>
2025-06-21 17:57:55 -0400

    [ODN-90] attempt to do array

--- opticsdotnet/opticsdotnet.Lib/Objects/PossibleValueSet/PossibleValueSet.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/PossibleValueSet/PossibleValueSet.cs
@@ -1,9 +1,19 @@
-﻿namespace opticsdotnet.Lib
+﻿using System.Numerics;
+
+namespace opticsdotnet.Lib
 {
     public abstract class PossibleValueSet<T>
     {
         public abstract int? NumItems { get; }//Null means infinity
         public abstract T FirstItem { get; }
         public abstract T LastItem { get; }
+    }
+
+    public static class PossibleValueSet_Extensions
+    {
+        public static BigInteger woooo<T>(this PossibleValueSet<T> sets)
+        {
+            return 1;
+        }
     }
 }
---
+++ opticsdotnet/opticsdotnet.Testing/Tasks/ODN90.cs
@@ -0,0 +1,55 @@
+﻿using opticsdotnet.Lib;
+using opticsdotnet.Lib.Mathematica;
+
+namespace opticsdotnet.Testing.ODN90
+{
+    public static class ODN90
+    {
+        public static void ODN90_1()
+        {
+            string saveDir = Path.Combine(Program.BaseSaveDir, "ODN-90");
+            Directory.CreateDirectory(saveDir);
+
+
+            var v1 = new RangePossibleValueSet(1, 2);
+            var v2 = new SingleValuePossibleValueSet<Circle2D>(new Circle2D(1, 2, 3));
+
+            var v3 = new PossibleValueSet<object>[]
+            {
+                v2,
+                v2
+            };
+
+
+
+            PossibleValueSet_Extensions.woooo()
+
+
+
+
+
+
+            return;
+
+
+
+
+
+
+
+            var dict = new Dictionary<string, Geo2D>();
+            string string1 = MathematicaUtil.DictionaryToMathematicaAssociation(dict);
+
+            dict.Add("Circle1", new Circle2D(0, 2, 1.3));
+            string string2 = MathematicaUtil.DictionaryToMathematicaAssociation(dict);
+
+            dict.Add("Circle2", new Circle2D(0, 4, 1.3));
+            string string3 = MathematicaUtil.DictionaryToMathematicaAssociation(dict);
+
+            dict.Add("C\nircle2", new Circle2D(0, 4, 1.3));
+            string string4 = MathematicaUtil.DictionaryToMathematicaAssociation(dict);
+
+            File.WriteAllLines(Path.Combine(saveDir, "ODN-54.txt"), [string1, string2, string3, string4]);
+        }
+    }
+}
