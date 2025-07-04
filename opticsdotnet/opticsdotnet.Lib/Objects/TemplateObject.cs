1b745e8fcbf23fb7b59ced9ed14bfbfa1c1873b3
Shane DiDona <shanedidona@gmail.com>
2025-07-04 18:36:22 -0400

    [ODN-90] add some code to public sealed class TemplateObject<T> : ITemplateSpot

--- opticsdotnet/opticsdotnet.Lib/Objects/TemplateObject.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/TemplateObject.cs
@@ -2,9 +2,11 @@
 {
     public sealed class TemplateObject<T> : ITemplateSpot
     {
-        
+        public readonly PossibleValueSet[] PossibleValueSets;
 
-
-
+        public TemplateObject(PossibleValueSet[] possibleValueSets)
+        {
+            PossibleValueSets = possibleValueSets;
+        }
     }
 }
