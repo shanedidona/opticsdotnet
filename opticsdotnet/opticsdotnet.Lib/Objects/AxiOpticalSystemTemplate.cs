dec663a64d53449fa4deccde512f53ee1bb0d3f4
Shane DiDona <shanedidona@gmail.com>
2025-07-04 21:59:27 -0400

    [ODN-90] some code for public class AxiOpticalSystemTemplate

---
+++ opticsdotnet/opticsdotnet.Lib/Objects/AxiOpticalSystemTemplate.cs
@@ -0,0 +1,61 @@
+﻿namespace opticsdotnet.Lib
+{
+    public class AxiOpticalSystemTemplate
+    {
+        public AxiOpticalSystemTemplate(
+                TemplateObject<AxiDrift>[] axiDriftTemplates,
+                TemplateObject<IAxiOpticalElement>[] axiOpticalElementTemplates
+            )
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+        public AxiOpticalSystemTemplate(
+            PossibleValueSet[] possibleValueSet,
+            IOpticalMaterial[] driftOpticalMaterials,
+            Type[] elementTypes
+            )
+        {
+
+
+
+
+
+        }
+
+
+
+
+
+        public static bool CheckValidity(ITemplateSpot[] tree)
+        {
+
+            throw new NotImplementedException();
+
+
+
+
+        }
+
+
+        public AxiOpticalSystem Generate1()
+        {
+            throw new NotImplementedException();
+        }
+            
+
+
+
+
+    }
+}
