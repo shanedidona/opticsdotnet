a625d661603b878f1d861ed2bb274d3a4ec4709f
Shane DiDona <shanedidona@gmail.com>
2025-07-06 16:32:44 -0400

    [ODN-90] get ctor

--- opticsdotnet/opticsdotnet.Lib/Objects/TemplateObject.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/TemplateObject.cs
@@ -1,12 +1,22 @@
-﻿namespace opticsdotnet.Lib
+﻿using System.Reflection;
+
+namespace opticsdotnet.Lib
 {
     public sealed class TemplateObject<T> : ITemplateSpot<T>
     {
+        readonly ConstructorInfo ConstructorInfo1;
         public readonly PossibleValueSet[] PossibleValueSets;
 
         public TemplateObject(PossibleValueSet[] possibleValueSets)
         {
             PossibleValueSets = possibleValueSets;
+
+            ConstructorInfo1 = typeof(T).GetConstructor(possibleValueSets.Select(x => x.ItemType).ToArray());
+
+            if (ConstructorInfo1 == null)
+            {
+                throw new NotSupportedException("ConstructorInfo1 == null");
+            }
         }
 
         public Type ItemType => typeof(T);
