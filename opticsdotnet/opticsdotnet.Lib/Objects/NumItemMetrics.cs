a51fa024e44a5f573a8fa014f9ff7332b602f3f3
Shane DiDona <shanedidona@gmail.com>
2025-07-06 21:03:52 -0400

    [ODN-90] copy comment

--- opticsdotnet/opticsdotnet.Lib/Objects/NumItemMetrics.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/NumItemMetrics.cs
@@ -4,7 +4,7 @@ namespace opticsdotnet.Lib
 {
     public sealed class NumItemMetricGroup
     {
-        public readonly BigInteger? NumItemsIncludingContinuous;
+        public readonly BigInteger? NumItemsIncludingContinuous;//Null means infinity
 
         public NumItemMetricGroup(BigInteger? numItemsIncludingContinuous)
         {
