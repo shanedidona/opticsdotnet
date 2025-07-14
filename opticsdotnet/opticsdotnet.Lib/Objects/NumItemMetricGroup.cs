b6fffdffdd2266dda9a5aeb4d4b89393f6c1c7c5
Shane DiDona <shanedidona@gmail.com>
2025-07-13 23:03:35 -0400

    [ODN-90] add comment

--- opticsdotnet/opticsdotnet.Lib/Objects/NumItemMetricGroup.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/NumItemMetricGroup.cs
@@ -22,7 +22,7 @@ namespace opticsdotnet.Lib
 
             foreach (NumItemMetricGroup numItemMetricGroup in groupsToMultiply)
             {
-                if (numItemMetricGroup.NumItemsIncludingContinuous.HasValue)
+                if (numItemMetricGroup.NumItemsIncludingContinuous.HasValue)//This is wrong since excluding cont does not always mean 1.
                 {
                     excludingContinuous *= numItemMetricGroup.NumItemsIncludingContinuous.Value;
                 }
