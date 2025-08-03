b2d5b84eacac986a7e14699218fe16a65b30d28c
Shane DiDona <shanedidona@gmail.com>
2025-08-03 15:38:07 -0400

    [ODN-90] readonly PossibleValueSet[][] JaggedPossibleValueSets;

--- opticsdotnet/opticsdotnet.Lib/Objects/AxiOpticalAssemblyTemplate.cs
+++ opticsdotnet/opticsdotnet.Lib/Objects/AxiOpticalAssemblyTemplate.cs
@@ -6,6 +6,7 @@
 
         readonly ITemplateSpot<AxiDrift>[] AxiDriftTemplates;
         readonly ITemplateSpot[] AxiOpticalElementTemplates;
+        readonly PossibleValueSet[][] JaggedPossibleValueSets;
 
         public AxiOpticalAssemblyTemplate(
                 ITemplateSpot<AxiDrift>[] axiDriftTemplates,
@@ -36,6 +37,23 @@
 
             AxiDriftTemplates = axiDriftTemplates;
             AxiOpticalElementTemplates = axiOpticalElementTemplates;
+
+            var jaggedPossibleValueSetsList = new List<PossibleValueSet[]>();
+            for (int i = 0; i < axiOpticalElementTemplates.Length; i++)
+            {
+                jaggedPossibleValueSetsList.Add(axiDriftTemplates[i].PossibleValueSets);
+                jaggedPossibleValueSetsList.Add(axiOpticalElementTemplates[i].PossibleValueSets);
+            }
+
+            jaggedPossibleValueSetsList.Add(axiDriftTemplates.Last().PossibleValueSets);
+
+            JaggedPossibleValueSets = jaggedPossibleValueSetsList.ToArray();
+
+
+
+
+
+
 
 
 
