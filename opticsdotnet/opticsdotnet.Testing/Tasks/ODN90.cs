ad990be79922a4a9a5baad83558063b8024b6f15
Shane DiDona <shanedidona@gmail.com>
2025-08-03 15:31:45 -0400

    Revert "[ODN-90] attempt change to ITemplateSpot<IAxiOpticalElement>[] axiOpticalElementTemplates = new ITemplateSpot<IAxiOpticalElement>[]"
    This reverts commit 496bccf0fc598803c36bdb2f99ce2cbc0939ba24.

--- opticsdotnet/opticsdotnet.Testing/Tasks/ODN90.cs
+++ opticsdotnet/opticsdotnet.Testing/Tasks/ODN90.cs
@@ -64,7 +64,7 @@ namespace opticsdotnet.Testing.ODN90
                 drift4Choices
             };
 
-            ITemplateSpot<IAxiOpticalElement>[] axiOpticalElementTemplates = new ITemplateSpot<IAxiOpticalElement>[]
+            ITemplateSpot[] axiOpticalElementTemplates = new ITemplateSpot[]
             {
                 someElements,
                 someElements2,
